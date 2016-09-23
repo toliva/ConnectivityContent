38408f235b3d8542491fd01a5226990496055162
Joshua King <jmking92@gmail.com>
2016-09-23 13:41:43 +1000

    Add environment settings, and move buttons to the bottom right

--- test-properties/urls.js
+++ test-properties/urls.js
@@ -1,4 +1,4 @@
-var hotelAssignmentServiceBaseUrl = "https://hotel-assignment-service.test.expedia.com";
+var hotelAssignmentServiceBaseUrl = environment.isProd() ? "https://hotel-assignment-service.prod-p.expedia.com" : "https://hotel-assignment-service.test.expedia.com";
 
 var hotelAssignmentServiceUrls = {
     login: hotelAssignmentServiceBaseUrl + "/v1/properties/login",
