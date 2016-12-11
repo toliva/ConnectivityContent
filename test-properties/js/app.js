1285321a981ee75895baac7169d1d8cd081381fc
Lindsay <lmurray@expedia.com>
2016-12-12 07:49:59 +1000

    Make unscheduled-admina requirejs module

---
+++ test-properties/js/app.js
@@ -0,0 +1,7 @@
+requirejs(['unscheduled-admin'],
+    function   (unscheduled) {
+        //jQuery, canvas and the app/sub module are all
+        //loaded and can be used here now.
+
+        unscheduled.init();
+    });
